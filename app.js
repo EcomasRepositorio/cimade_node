b54f3641fb97351c36595e4b795458f3ca377dc2
jeferson <jefersonyanac29@gmail.com>
2023-09-10 11:04:58 -0500

    config https

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const path = require('path');
 
@@ -7,11 +9,19 @@ app.use(express.json());
 app.use(cors()); 
 app.use('/', require('./router'));
 app.use(express.static('public'));
-
 app.use('/pdfs', express.static(path.join(__dirname, 'PDF_ECOMAS')));
 
-app.listen(4000, () => {
-  app.on("listen",function(){
-    console.log('SERVER Running in http://localhost:4000');
-  })
+// Configuración de certificado y clave
+const certificatePath = '/etc/letsencrypt/live/ecomas.pe/fullchain.pem';
+const privateKeyPath = '/etc/letsencrypt/live/ecomas.pe/privkey.pem';
+const credentials = {
+  key: fs.readFileSync(privateKeyPath),
+  cert: fs.readFileSync(certificatePath),
+};
+
+// Crear servidor HTTPS
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(4000, () => {
+  console.log('HTTPS SERVER Running in https://localhost:4000');
 });
